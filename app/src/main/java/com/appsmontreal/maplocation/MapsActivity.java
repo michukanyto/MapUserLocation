31988d01fa2040db81b05f5bd1f38518f92415cd
Marlon <escobar.marlon@gmail.com>
2019-08-07 12:42:58 -0400

    Updating icon and zoom

--- app/src/main/java/com/appsmontreal/maplocation/MapsActivity.java
+++ app/src/main/java/com/appsmontreal/maplocation/MapsActivity.java
@@ -2,18 +2,24 @@ package com.appsmontreal.maplocation;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +45,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+//        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng userPosition = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(userPosition).title("Tomas' position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));//After icon it's just to personalize the icon, that's not mandatory
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition,20));
     }
 }
