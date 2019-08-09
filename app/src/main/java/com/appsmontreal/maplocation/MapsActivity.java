9dd8c06c86be4079aafb92209d78c3afb537e358
Marlon <escobar.marlon@gmail.com>
2019-08-09 18:01:19 -0400

    Toast showing Up Address

--- app/src/main/java/com/appsmontreal/maplocation/MapsActivity.java
+++ app/src/main/java/com/appsmontreal/maplocation/MapsActivity.java
@@ -8,11 +8,15 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +25,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -122,5 +130,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng userPosition = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(userPosition).title("Tomas' position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));//After icon it's just to personalize the icon, that's not mandatory
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition,19));
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(latitude,longitude,1);
+
+            if (listAddresses != null && listAddresses.size() > 0) {
+                String anAddress = "";
+                if (listAddresses.get(0).getThoroughfare() != null) {
+                    anAddress += listAddresses.get(0).getThoroughfare() + " ";
+                }
+
+                if (listAddresses.get(0).getAdminArea() != null) {
+                    anAddress += listAddresses.get(0).getAdminArea() + " ";
+                }
+
+                if (listAddresses.get(0).getLocality() != null) {
+                    anAddress += listAddresses.get(0).getLocality();
+                }
+                Toast.makeText(MapsActivity.this,anAddress,Toast.LENGTH_LONG).show();
+                Log.i("Location Data ===> ", listAddresses.get(0).toString());
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
